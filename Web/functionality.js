81ccef6fa599fa9c417acc8cf66ac894f76af5f7
mmichaud93 <mmichaud93@gmail.com>
2014-07-30 20:06:42 -0400

    web stuff

---
+++ Web/functionality.js
@@ -0,0 +1,12 @@
+window.onload = function() {
+	$(".helix-index-item").click(function() {
+		console.log($(this).attr("data"));
+		$.post( './card.php', { 'data' : $(this).attr("data") }, function() {
+    		window.location.href = './card.php';
+		});
+	});
+
+	$(".helix-index-get-started").click(function() {
+		window.location.href = './add.html';
+	});
+}
